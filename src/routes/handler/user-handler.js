b3213de77afe167911a194c6aa57cac185537c60
kamal <lamgadekamal@gmail.com>
2022-10-31 22:21:47 +1300

    implement user register endpoint and add debug launch

--- src/routes/handler/user-handler.js
+++ src/routes/handler/user-handler.js
@@ -1,72 +1,89 @@
-const logger = require('../../utils/logger');
-const ApiHandler = require('../../common/api-handler');
-const UserRepository = require('../../repositories/user-repository');
-const { validationResult } = require('express-validator');
-const gravator = require('gravator');
-const bcrypt = require('bcryptjs');
-
+const logger = require("../../utils/logger");
+const ApiHandler = require("../../common/api-handler");
+const UserRepository = require("../../repositories/user-repository");
+const User = require("../../models/user");
+const { validationResult } = require("express-validator");
+const gravator = require("gravator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
 
 class UserHandlers extends ApiHandler {
   constructor(logger) {
     super(logger);
     this.userRepository = new UserRepository(logger);
     this.getUsers = this.getUsers.bind(this);
-    this.createUser = this.createUser.bind(this);
+    this.registerUser = this.registerUser.bind(this);
   }
 
   async getUsers(req, res) {
     try {
       const {
-        direction = 'asc',
-        sort = 'name',
+        direction = "asc",
+        sort = "name",
         page = 0,
-        perPage = 50
+        perPage = 50,
       } = req.query;
       let filter = {};
       logger.info(`accessing to user data source repository`);
-      let result = await this.userRepository.paged(filter, page, perPage)
+      let result = await this.userRepository.paged(filter, page, perPage);
       res.status(200);
       res.json({
         data: result,
         metadata: {
           page: Number(page),
           perPage: Number(perPage),
-          total: result.total
-        }
+          total: result.total,
+        },
       });
     } catch (error) {
-      logger.info('error', error);
+      logger.info("error", error);
     }
   }
 
-  async createUser(req, res) {
+  async registerUser(req, res) {
     try {
       const input = req.body;
       if (input) {
         //payload validation
         const errors = validationResult(req);
-        if(!errors.isEmpty) {
-          return res.status(400).json({ errors : errors.array() });
+        if (!errors.isEmpty) {
+          return res.status(400).json({ errors: errors.array() });
         }
         //validate if user exists
-        const { name, email, password } = req.body;
+        const { name, email, password, avatar } = req.body;
         let user = await this.userRepository.getUser(email);
-        if(user) { res.status(400).json({ errors : [{ msg : 'User already exists. '}]}) };
-        //get gravatar 
-       // const avatar  = gravator.url(email, { s: '200', r: 'pg', d: 'mm' });
-        //encrypt password
-        //const salt = await bcrypt.genSalt(10);
-        //user.password = await bcrypt.hash(password, salt);
+        if (user) {
+          res.status(400).json({ errors: [{ msg: "User already exists. " }] });
+        }
 
-        //create user object
-        let result = await this.userRepository.create(input);
-        res.status(200);
-        res.json({
-          data: result.toObject()
-        });
-      } else {
-        res.status(400);
-        res.json({ data: null });
+        //TODO: get gravatar
+        //const avatar = gravator.url(email, { s: "200", r: "pg", d: "mm" });
+        user = new User({ name, email, avatar, password });
+
+        //encrypt password
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        //await user.save();
+        await this.userRepository.create(input);
+
+        //jwt token
+        const payload = {
+          user: { id: user.id },
+        };
+
+        jwt.sign(
+          payload,
+          config.get("jwtSecret"),
+          { expiresIn: 360000 },
+          (err, token) => {
+            if (err) {
+              logger.info("error", err);
+              throw err;
+            }
+            res.json({ token });
+          }
+        );
       }
     } catch (err) {
       this.handleError(err, res);
--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -13,5 +13,5 @@ module.exports = (app, log) => {
       check('name', 'Name is required').not().isEmpty(),
       check('email', 'Please include a valid email').isEmail(),
       check('password', 'Please enter a password with 6 or more characters').isLength({ min : 6 })
-    ], handlers.createUser);
+    ], handlers.registerUser);
 }
