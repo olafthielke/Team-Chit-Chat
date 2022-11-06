01daf797953a6c9dd085fca4b938b749e4baae8e
kamal <lamgadekamal@gmail.com>
2022-11-06 22:24:48 +1300

    React: add navbar and landing component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import { Fragment } from 'react';
+import Navbar from './components/Navbar';
+import Landing from './components/Landing';
 import './App.css';
 
 const App = () => (
   <Fragment>
     <h1>App</h1>
+    <Navbar></Navbar>
+    <Landing></Landing>
   </Fragment>
 );
 
