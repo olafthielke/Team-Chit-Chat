b4fa05d3d6a597829360125ee5cf43847302c58b
kamal <lamgadekamal@gmail.com>
2022-11-06 21:36:11 +1300

    script to run server and client concurrently

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(authRoutes);
 authRoutes(app.request, app.response, (response) => { console.log("auth routes response", response) });
 
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 dbPromise.then(() => {
     server.listen(PORT, () => {
