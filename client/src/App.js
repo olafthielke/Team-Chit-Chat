f919966d0fc0d9a80020707655329c7f15fa964a
kamal <lamgadekamal@gmail.com>
2023-01-16 23:01:10 +1300

    router setup: used router, routes and link

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,20 @@
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Landing from "./components/Landing";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
 import "./App.css";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import { Fragment } from "react";
 
 const App = () => (
   <Router>
-    <Navbar/>
+    <Fragment>
+      <Navbar />
+    </Fragment>
     <Routes>
-      <Route  path="/" element={<Landing />} />
+      <Route path="/" element={<Landing />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   </Router>
 );
