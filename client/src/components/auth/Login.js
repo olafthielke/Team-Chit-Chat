f00855d12ab9f2dedc031748a281597666b71329
kamal <lamgadekamal@gmail.com>
2023-03-19 21:14:02 +1300

    add login page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,11 +1,61 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
 
 const Login = () => {
-  return (
-    <div>
-      Login
-    </div>
-  )
-}
+  //add useState hook to capture formData object states
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
 
-export default Login
+  const { email, password } = formData;
+
+  //add custom onChange and onSubmit(form submission) func
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    console.log("Login success");
+  };
+
+  return (
+    <section className="container">
+      <h1 className="large text-primary">Login</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Sign Into Your Account
+      </p>
+      <form className="form" onSubmit={(e) => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={(e) => onChange(e)}
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </section>
+  );
+};
+
+export default Login;
+
