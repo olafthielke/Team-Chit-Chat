8876e802d127f291feea01ff280a79b76a0be66b
olafthielke <olaf.thielke@gmail.com>
2023-05-19 16:19:13 +1200

    Add test for empty request body.

--- src/routes/handler/user-handler.test.js
+++ src/routes/handler/user-handler.test.js
@@ -3,3 +3,8 @@ const UserHandler = require("./user-handler");
 test("Can construct UserHandler", () => {
     const handler = new UserHandler();
 });
+
+test("Given empty request body When call registerUser Then don't do anything", async () => {
+    const handler = new UserHandler();
+    await handler.registerUser({ }, null);
+});
